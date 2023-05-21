4308f73d024b5719f7a84af9cf5ed7a9f8114269
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-22 00:27:24 +0300

    Done with assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, name , rating):
+        self.title =title
+        self.name = name
+        self.rating = rating
+    def introduceBook(self):
+        print(self.title, self.name, self.rating)
+    def addBookRating(self):
+        self.rating = int(self.rating) + 1
+    def reduceRating(self):
+        self.rating = int(self.rating) -1
+        
+class Author(Book):
+    def __init__(self, title, name, rating,fname, lname):
+        super().__init__(title, name, rating)
+        self.fname =fname 
+        self.lname = lname
+    def introduceBookByAuthor(self):
+        print(self.fname, self.lname, self.title, self.rating,self.name)
+
+dollsHouse = Author("A doll's House", "Henrik Ibsen", "1", "Henrik", "Ibsen")
+Author.introduceBook()
+
+
     
         
