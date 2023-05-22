aa5575e6fb51ccbfc47d37a6634a77e28b22c1dc
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-22 08:12:44 +0300

    solved

--- lib/book.py
+++ lib/book.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title, name , rating):
-        self.title =title
-        self.name = name
-        self.rating = rating
-    def introduceBook(self):
-        print(self.title, self.name, self.rating)
-    def addBookRating(self):
-        self.rating = int(self.rating) + 1
-    def reduceRating(self):
-        self.rating = int(self.rating) -1
-        
-class Author(Book):
-    def __init__(self, title, name, rating,fname, lname):
-        super().__init__(title, name, rating)
-        self.fname =fname 
-        self.lname = lname
-    def introduceBookByAuthor(self):
-        print(self.fname, self.lname, self.title, self.rating,self.name)
-
-dollsHouse = Author("A doll's House", "Henrik Ibsen", "1", "Henrik", "Ibsen")
-Author.introduceBook()
+    def __init__(self, tittle, author, year ):
+        self.tittle = tittle
+        self.author = author
+        self.year = year
+    def likesOfReader(self):
+        print("i like reading", self.tittle, "written by", self.author, "in the year", self.year)
+    
+article = Book ("A Doll's House", "Calvin Arieri", 1983)
+article.likesOfReader()
 
 
     
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__ (self, brand, color):
+        self.brand = brand
+        self.color = color
+
+    def whoIsWearing(self):
+        print ("I am wearing a ", self.brand, " ", self.color, " shoe")
+airforce = Shoe("Nike", "Black")
+
+airforce.whoIsWearing()
+
+class Soal (Shoe):
+    def __init__(self, brand, color, make, size):
+        super().__init__(brand, color)
+        self.size = size
+        self.make = make
+
+    def whoIsWearing(self):
+        print(f"i am wearing a shoe of {self.brand}, it is color {self.color} and of make {self.make}, and it is number {self.size} in size")
+
+airforce = Soal("Nike", "Blue", "Air Force", 4)
+airforce.whoIsWearing()
